2871ae276fcb11f8959cfd29fc898d39bd4cf309
adieunix <adie.rock@yahoo.com>
2018-10-02 15:41:54 +0700

    second commit

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 const connect = require('connect');
 const express = require('express');
 const bodyParser = require('body-parser');
+const serveStatic = require('serve-static');
 const vhost = require('vhost');
 const app = express();
 
-app.use(express.static('public'));
+/* STG */
+var stg = connect();
+stg.use(serveStatic('public/stg/www/'));
+
+/* LIVE */
+var live = connect();
+live.use(serveStatic('public/live/'));
+
 app.use(bodyParser.json());
-app.set('view engine', 'ejs');
-
-/* STG.ID */
-var stg = express();
-stg.use(function(req, res){
-    if (!module.parent) console.log(req.vhost);
-    res.end('Welcome to STG');
-});
-
-/* LIVE.ID */
-var live = express();
-live.use(function(req, res){
-    if (!module.parent) console.log(req.vhost);
-    res.end('Welcome to LIVE');
-});
 
 /* VHOST */
 app.use(vhost('stg.mitelor.id', stg)); 
